0e632ed195f763437f9b52eba3dfea713528eb0d
Mat0u5 <matous.danko@gmail.com>
2026-09-23 20:57:11 +0200

    FTB update

--- common/src/main/java/com/anthonyhilyard/merchantmarkers/mixin/FTBChunksClientMixin.java
+++ common/src/main/java/com/anthonyhilyard/merchantmarkers/mixin/FTBChunksClientMixin.java
@@ -4,30 +4,20 @@ import com.anthonyhilyard.merchantmarkers.compat.FTBChunksHandler;
 import com.anthonyhilyard.merchantmarkers.render.Markers;
 import com.anthonyhilyard.merchantmarkers.config.MerchantMarkersConfig;
 
+import dev.ftb.mods.ftbchunks.client.FTBChunksClient;
+import dev.ftb.mods.ftblibrary.icon.EntityIconLoader;
+import dev.ftb.mods.ftblibrary.icon.Icon;
+import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import dev.ftb.mods.ftbchunks.client.mapicon.EntityMapIcon;
-import dev.ftb.mods.ftbchunks.client.FTBChunksClient;
-import dev.ftb.mods.ftbchunks.api.client.icon.MapType;
-import dev.ftb.mods.ftbchunks.api.client.icon.MapIcon;
-import dev.ftb.mods.ftblibrary.icon.Icon;
-
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.Identifier;
 
 @Mixin(FTBChunksClient.class)
 public class FTBChunksClientMixin
 {
-	private final static Identifier VILLAGER_LOCATION = Identifier.withDefaultNamespace("villager");
-
-	@Redirect(method = "mapIcons", at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbchunks/client/mapicon/EntityIcons;get(Lnet/minecraft/world/entity/Entity;)Ldev/ftb/mods/ftblibrary/icon/Icon;"), require = 0)
-	public Icon redirectGet(Entity entity)
+	@Redirect(method = "onMapIconEvent", at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftblibrary/icon/EntityIconLoader;getIcon(Lnet/minecraft/world/entity/Entity;)Ldev/ftb/mods/ftblibrary/icon/Icon;"), require = 0)
+	public Icon<?> redirectGetIcon(Entity entity)
 	{
 		// If this is a villager, return the dynamic texture instead of the default one.
 		if (MerchantMarkersConfig.getInstance().showOnMiniMap.get() && Markers.shouldShowMarker(entity))
@@ -35,38 +25,6 @@ public class FTBChunksClientMixin
 			return Icon.getIcon(FTBChunksHandler.villagerTexture);
 		}
 
-		return EntityIcons.get(entity);
-	}
-
-	@Redirect(method = "mapIcons", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/EntityType;getCategory()Lnet/minecraft/world/entity/MobCategory;"), require = 0)
-	public MobCategory redirectGetCategory(EntityType<?> entityType)
-	{
-		if (MerchantMarkersConfig.getInstance().showOnMiniMap.get())
-		{
-			// Redirect the getCategory call to tell FTB Chunks that villagers aren't "Misc" category (so they aren't skipped).
-			if (BuiltInRegistries.ENTITY_TYPE.getKey(entityType).equals(VILLAGER_LOCATION))
-			{
-				return MobCategory.CREATURE;
-			}
-		}
-
-		return entityType.getCategory();
-	}
-
-	@Redirect(method = "renderHud", at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbchunks/api/client/icon/MapIcon;draw(Ldev/ftb/mods/ftbchunks/api/client/icon/MapType;Lnet/minecraft/client/gui/GuiGraphics;IIIIZI)V"), require = 0)
-	public void redirectIconDraw(MapIcon icon, MapType mapType, GuiGraphics graphics, int x, int y, int w, int h, boolean outsideVisibleArea, int iconAlpha)
-	{
-		if (icon instanceof EntityMapIcon entityIcon)
-		{
-			Entity entity = FTBChunksHandler.getEntityFromIcon(entityIcon);
-
-			// If this is a villager, return the dynamic texture instead of the default one.
-			if (MerchantMarkersConfig.getInstance().showOnMiniMap.get() && Markers.shouldShowMarker(entity))
-			{
-				FTBChunksHandler.setCurrentEntity(entity);
-			}
-		}
-
-		icon.draw(mapType, graphics, x, y, w, h, outsideVisibleArea, iconAlpha);
+		return EntityIconLoader.getIcon(entity);
 	}
 }
--- common/src/main/java/com/anthonyhilyard/merchantmarkers/mixin/FTBChunksIconLayerRendererMixin.java
+++ common/src/main/java/com/anthonyhilyard/merchantmarkers/mixin/FTBChunksIconLayerRendererMixin.java
@@ -1,25 +1,25 @@
 package com.anthonyhilyard.merchantmarkers.mixin;
 
-import com.anthonyhilyard.merchantmarkers.config.MerchantMarkersConfig;
 import com.anthonyhilyard.merchantmarkers.compat.FTBChunksHandler;
 import com.anthonyhilyard.merchantmarkers.render.Markers;
+import com.anthonyhilyard.merchantmarkers.config.MerchantMarkersConfig;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import dev.ftb.mods.ftbchunks.client.mapicon.EntityMapIcon;
-import dev.ftb.mods.ftbchunks.client.gui.map.MapIconWidget;
+import dev.ftb.mods.ftbchunks.client.minimap.layers.IconLayerRenderer;
 import dev.ftb.mods.ftbchunks.api.client.icon.MapType;
 import dev.ftb.mods.ftbchunks.api.client.icon.MapIcon;
 
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.entity.Entity;
 
-@Mixin(MapIconWidget.class)
-public class FTBChunksMapIconWidgetMixin
+@Mixin(IconLayerRenderer.class)
+public class FTBChunksIconLayerRendererMixin
 {
-	@Redirect(method = "draw", at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbchunks/api/client/icon/MapIcon;draw(Ldev/ftb/mods/ftbchunks/api/client/icon/MapType;Lnet/minecraft/client/gui/GuiGraphics;IIIIZI)V", remap = false), remap = false, require = 0)
+	@Redirect(method = "renderLayer", at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbchunks/api/client/icon/MapIcon;draw(Ldev/ftb/mods/ftbchunks/api/client/icon/MapType;Lnet/minecraft/client/gui/GuiGraphics;IIIIZI)V", remap = false), remap = false, require = 0)
 	public void redirectIconDraw(MapIcon icon, MapType mapType, GuiGraphics graphics, int x, int y, int w, int h, boolean outsideVisibleArea, int iconAlpha)
 	{
 		if (icon instanceof EntityMapIcon entityIcon)
